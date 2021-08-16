aaa728b63e384b68d4ecba16b20cb7cae1aead81
Cesar Payan <cesarpayan@microsoft.com>
2021-08-16 10:45:43 -0700

    UnsafeEncode optimization

--- src/Microsoft.IdentityModel.Tokens/Base64UrlEncoder.cs
+++ src/Microsoft.IdentityModel.Tokens/Base64UrlEncoder.cs
@@ -111,60 +111,46 @@ namespace Microsoft.IdentityModel.Tokens
 #if !NET45
         private unsafe static string UnsafeEncode(string str)
         {
-            bool needReplace = false;
-            int reductionSize = 0;
-            if (str[str.Length - 1] == base64PadCharacter)
-                reductionSize = 1;
+            int length = str.Length;
 
-            if (str[str.Length - 2] == base64PadCharacter)
-                reductionSize = 2;
-
-            int encodedLength = str.Length - reductionSize;
-            for (int i = 0; i < str.Length; i++)
+            if (str[length - 1] == base64PadCharacter)
             {
-                if (str[i] == base64Character62 || str[i] == base64Character63)
+                int reductionSize = 1;
+
+                if (str[length - 2] == base64PadCharacter)
                 {
-                    needReplace = true;
-                    break;
+                    reductionSize = 2;
                 }
+
+                str = str.Substring(0, length - reductionSize);
             }
 
-            if (needReplace)
+            fixed (char* pSrc = str)
             {
-                string encodedString = new string(char.MinValue, encodedLength);
-                fixed (char* dest = encodedString)
-                {
-                    for (int i = 0; i < encodedLength; i++)
-                    {
-                        if (str[i] == base64Character62)
-                            dest[i] = base64UrlCharacter62;
-                        else if (str[i] == base64Character63)
-                            dest[i] = base64UrlCharacter63;
-                        else
-                            dest[i] = str[i];
-                    }
-                }
+                char* end = pSrc + str.Length;
+                char* p = pSrc;
 
-                return encodedString;
-            }
-            else
-            {
-                if (encodedLength == str.Length)
+                while (p < end)
                 {
-                    return str;
-                }
-                else
-                {
-                    string encodedString = new string(char.MinValue, encodedLength);
-                    fixed (char* src = str)
-                    fixed (char* dest = encodedString)
+                    char ch = *p;
+
+                    if (ch < '0')
                     {
-                        Buffer.MemoryCopy(src, dest, encodedLength * 2, encodedLength * 2);
+                        if (ch == base64Character62)
+                        {
+                            *p = base64UrlCharacter62;
+                        }
+                        else if (ch == base64Character63)
+                        {
+                            *p = base64UrlCharacter63;
+                        }
                     }
 
-                    return encodedString;
+                    p++;
                 }
             }
+
+            return str;
         }
 #endif
 
