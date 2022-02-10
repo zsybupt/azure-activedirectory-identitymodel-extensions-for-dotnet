984667cd5ee923621d500255726d0dc597d8edb7
Siyuan Zheng <siyzhe@microsoft.com>
2022-02-10 20:33:25 +0800

    add comments and fix bug

--- src/Microsoft.IdentityModel.Tokens/Base64UrlEncoder.cs
+++ src/Microsoft.IdentityModel.Tokens/Base64UrlEncoder.cs
@@ -61,10 +61,10 @@ namespace Microsoft.IdentityModel.Tokens
         {
             -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
             -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, 62, -1, 63,         // 62 is placed at index 43 (for +), 63 at index 47 (for /)
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, 62, -1, 63,         // 62 is placed at index 43 (for +) and 45 (for -), 63 at index 47 (for /)
             52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,         // 52-61 are placed at index 48-57 (for 0-9), 64 at index 61 (for =)
             -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, 63,         // 0-25 are placed at index 65-90 (for A-Z)
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, 63,         // 0-25 are placed at index 65-90 (for A-Z), 63 at 95 (for /)
             -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
             41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1,         // 26-51 are placed at index 97-122 (for a-z)
             -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,         // Bytes over 122 ('z') are invalid and cannot be decoded
@@ -277,7 +277,7 @@ namespace Microsoft.IdentityModel.Tokens
                 int i1 = str[encodedIndex + 1];
                 int i2 = str[encodedIndex + 2];
 
-                if (((i0 | i1) & 0xffffff00) != 0)
+                if (((i0 | i1 | i2) & 0xffffff00) != 0)
                 {
                     throw LogHelper.LogExceptionMessage(new FormatException("bad format base64urlencoded string"));
                 }
